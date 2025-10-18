369af4ca7277d77072af729f175e7f5abe4ec8b9
omonubi <omonubi@hotmail.com>
2025-10-18 07:02:23 -0700

    251018.0702 movement scripts

--- api/nextphase.js
+++ api/nextphase.js
@@ -3,18 +3,18 @@
 // game: FASA Star Trek: STCS
 // description: A script to advance a dedicated phase-tracking token through
 //  various phases of a combat turn. Helps players keep track of where they
-//  are in the combat turn.
+//  aew in the combat turn.
 
 var PhaseIndex = 0;
 const names = ['Phase 1: Movement', 'Phase 1: Targeting', 'Phase 1: Fire', 'Phase 1: Recharge', 'Phase 2: Movement', 'Phase 2: Targeting', 'Phase 2: Fire', 'Phase 2: Recharge', 'Phase 3: Movement', 'Phase 3: Targeting', 'Phase 3: Fire', 'Phase 3: Recharge', 'Phase/Turn is COMPLETE!'];
 
 // Reset name of token and phase counter
 on('chat:message', function(msg) {
-    if(msg.type == 'api' && msg.content.indexOf('!nextphase') == 0){
+    if (msg.type == 'api' && msg.content.indexOf('!nextphase') == 0){
 		const selectedObjs = findObjs({type: 'graphic'});
 		
-		_.each(selectedObjs, function(obj) {
-			if(obj.get('_subtype') == 'token' && obj.get('name').startsWith('Phase')) {
+		_.each (selectedObjs, function(obj) {
+			if (obj.get('_subtype') == 'token' && obj.get('name').startsWith('Phase')) {
 			    obj.set('name', names[0]);
 	            sendChat("Phase Tracker", names[0]);
 			    PhaseIndex = 1;
@@ -30,11 +30,23 @@ on('change:campaign:turnorder', function() {
     const myTokenId = myToken['id'];
   
     const myObj = getObj('graphic', myTokenId);
-    if(myObj.get('name').startsWith('Phase')) {
+    if (myObj.get('name').startsWith('Phase')) {
 	    myObj.set('name', names[PhaseIndex]);
 	    sendChat("Phase Tracker", names[PhaseIndex]);
 	    sortTurnOrder(sorter_asc);
+	    
+	    if (PhaseIndex == 3 || PhaseIndex == 7 || PhaseIndex == 11) removeFireTokens();
+	    
 	    PhaseIndex ++;
-	    if(PhaseIndex == 13) PhaseIndex = 0;
+	    if (PhaseIndex == 13) PhaseIndex = 0;
     }
 });
+
+// Remove fire tokens from map
+function removeFireTokens() {
+	const selectedObjs = findObjs({type: 'graphic'});
+	
+	_.each (selectedObjs, function(obj) {
+		if (obj.get('_subtype') == 'card') { obj.remove(); }
+	});
+}
--- api/stcsmove.js
+++ api/stcsmove.js
@@ -1,48 +1,104 @@
-
-// Move forward
+// Move forward 1 hex
 on('chat:message', function(msg) {
-    if(msg.type == 'api' && msg.selected && msg.content.indexOf('!move') == 0){
+    if(msg.type == 'api' && msg.selected && msg.content.indexOf('!move 1') == 0){
 		var selectedObjs = msg.selected;
 		_.each(selectedObjs, function(obj) {
 			if(obj._type == 'graphic'){
 				var token = getObj('graphic', obj._id);
-                var top = token.get("top");                 //Find the current top position value
+                var top = token.get("top");
                 var left = token.get("left");
                 var rotation = token.get("rotation");
-                top = top - Math.cos(rotation * Math.PI / 180) * 200;                             //Change position
-                left = left + Math.sin(rotation * Math.PI / 180) * 200;
-                token.set({top: top, left: left});                      //Set new position
+                top = top - Math.cos(rotation * Math.PI / 180) * 70;
+                left = left + Math.sin(rotation * Math.PI / 180) * 70;
+                token.set({top: top, left: left});
 			};
 		});
 	};
 });
 
-// Turn right
+// Move forward 2 hexes
 on('chat:message', function(msg) {
-    if(msg.type == 'api' && msg.selected && msg.content.indexOf('!turnr') == 0){
+    if(msg.type == 'api' && msg.selected && msg.content.indexOf('!move 2') == 0){
 		var selectedObjs = msg.selected;
 		_.each(selectedObjs, function(obj) {
 			if(obj._type == 'graphic'){
 				var token = getObj('graphic', obj._id);
-        		var rotation = token.get("rotation");   //Find the current rotation value
-        		rotation = rotation + 22.5;             //add 22.5 degrees 
-        		token.set({rotation: rotation});        //set the rotation
+                var top = token.get("top");
+                var left = token.get("left");
+                var rotation = token.get("rotation");
+                top = top - Math.cos(rotation * Math.PI / 180) * 140;
+                left = left + Math.sin(rotation * Math.PI / 180) * 140;
+                token.set({top: top, left: left});
 			};
 		});
 	};
 });
 
-// Turn left
-*/
+// Rotate 1 hex facing to port
 on('chat:message', function(msg) {
-    if(msg.type == 'api' && msg.selected && msg.content.indexOf('!turnl') == 0){
+    if(msg.type == 'api' && msg.selected && msg.content.indexOf('!move rp') == 0){
 		var selectedObjs = msg.selected;
 		_.each(selectedObjs, function(obj) {
 			if(obj._type == 'graphic'){
 				var token = getObj('graphic', obj._id);
-                var rotation = token.get("rotation");   //Find the current rotation value
-        		rotation = rotation - 22.5;             //add 22.5 degrees 
-        		token.set({rotation: rotation});        //set the rotation
+        		var rotation = token.get("rotation");
+        		rotation = rotation - 60;
+        		token.set({rotation: rotation});
+			};
+		});
+	};
+});
+
+// Rotate 1 hex facing to starboard
+on('chat:message', function(msg) {
+    if(msg.type == 'api' && msg.selected && msg.content.indexOf('!move rs') == 0){
+		var selectedObjs = msg.selected;
+		_.each(selectedObjs, function(obj) {
+			if(obj._type == 'graphic'){
+				var token = getObj('graphic', obj._id);
+                var rotation = token.get("rotation");
+        		rotation = rotation + 60;
+        		token.set({rotation: rotation});
+			};
+		});
+	};
+});
+
+// Sideslip to port
+on('chat:message', function(msg) {
+    if(msg.type == 'api' && msg.selected && msg.content.indexOf('!move ssp') == 0){
+		var selectedObjs = msg.selected;
+		_.each(selectedObjs, function(obj) {
+			if(obj._type == 'graphic'){
+				var token = getObj('graphic', obj._id);
+                var top = token.get("top");
+                var left = token.get("left");
+                var rotation = token.get("rotation");
+                top = top - Math.cos((rotation - 60) * Math.PI / 180) * 70; 
+                top = top - Math.cos(rotation * Math.PI / 180) * 70;
+                left = left + Math.sin((rotation - 60) * Math.PI / 180) * 70;
+                left = left + Math.sin(rotation * Math.PI / 180) * 70;
+                token.set({top: top, left: left});
+			};
+		});
+	};
+});
+
+// Sideslip to starboard
+on('chat:message', function(msg) {
+    if(msg.type == 'api' && msg.selected && msg.content.indexOf('!move sss') == 0){
+		var selectedObjs = msg.selected;
+		_.each(selectedObjs, function(obj) {
+			if(obj._type == 'graphic'){
+				var token = getObj('graphic', obj._id);
+                var top = token.get("top");
+                var left = token.get("left");
+                var rotation = token.get("rotation");
+                top = top - Math.cos((rotation + 60) * Math.PI / 180) * 70; 
+                top = top - Math.cos(rotation * Math.PI / 180) * 70;
+                left = left + Math.sin((rotation + 60) * Math.PI / 180) * 70;
+                left = left + Math.sin(rotation * Math.PI / 180) * 70;
+                token.set({top: top, left: left});
 			};
 		});
 	};
