4b2d967258b00d91055f94146e39774680f98417
omonubi <omonubi@hotmail.com>
2025-10-23 08:42:48 -0700

    251023.0842 nextphase updates

--- api/nextphase.js
+++ api/nextphase.js
@@ -3,14 +3,21 @@
 // game: FASA Star Trek: STCS
 // description: A script to advance a dedicated phase-tracking token through
 //  various phases of a combat turn. Helps players keep track of where they
-//  aew in the combat turn.
+//  arw in the combat turn.
+// note: updates a customized version of TurnMarker1 at the end of each round
 
 var PhaseIndex = 0;
+var RoundIndex = 1; // Used to track combat rounds
 const names = ['Phase 1: Movement', 'Phase 1: Targeting', 'Phase 1: Fire', 'Phase 1: Recharge', 'Phase 2: Movement', 'Phase 2: Targeting', 'Phase 2: Fire', 'Phase 2: Recharge', 'Phase 3: Movement', 'Phase 3: Targeting', 'Phase 3: Fire', 'Phase 3: Recharge', 'Phase/Turn is COMPLETE!'];
 
 // Reset name of token and phase counter
 on('chat:message', function(msg) {
-    if (msg.type == 'api' && msg.content.indexOf('!nextphase') == 0){
+    // New combat 
+    if (msg.type == 'api' && msg.content.indexOf('!nextphase reset') == 0){
+        PhaseIndex = 0;
+        RoundIndex = 1;
+    // New combat round
+    } else if (msg.type == 'api' && msg.content.indexOf('!nextphase') == 0){
 		const selectedObjs = findObjs({type: 'graphic'});
 		
 		_.each (selectedObjs, function(obj) {
@@ -18,12 +25,13 @@ on('chat:message', function(msg) {
 			    obj.set('name', names[0]);
 	            sendChat("Phase Tracker", names[0]);
 			    PhaseIndex = 1;
+			    RoundIndex ++;
 			}
 		});
 	};
 });
 
-// Advance phase
+// Advance to the next combat phase
 on('change:campaign:turnorder', function() {
     const allTokens = getTurnArray();
     const myToken = allTokens[0];
@@ -33,12 +41,29 @@ on('change:campaign:turnorder', function() {
     if (myObj.get('name').startsWith('Phase')) {
 	    myObj.set('name', names[PhaseIndex]);
 	    sendChat("Phase Tracker", names[PhaseIndex]);
-	    sortTurnOrder(sorter_asc);
-	    
-	    if (PhaseIndex == 3 || PhaseIndex == 7 || PhaseIndex == 11) removeFireTokens();
-	    
-	    PhaseIndex ++;
-	    if (PhaseIndex == 13) PhaseIndex = 0;
+	    switch (PhaseIndex) {
+	        case 2: case 3: case 6: case 7: case 10: case 11:
+	            sortTurnOrder(sorter_desc);
+	        case 3: case 7: case 11:
+	            removeFireTokens();
+	            break;
+	        default:
+	            sortTurnOrder(sorter_asc);
+        }
+        PhaseIndex ++;
+    }
+    // If the round is over, update Round label in turn tracker
+    if (PhaseIndex == 13) {
+        PhaseIndex = 0;
+        const resortedTokens = getTurnArray();
+        for (let i = 0; i < resortedTokens.length; i++) {
+            const aToken = resortedTokens[i];
+            const aTokenId = aToken['id'];
+            const myObj = getObj('graphic', aTokenId);
+            if (myObj.get('name').startsWith('Round')) {
+                myObj.set('name', 'Round ' + RoundIndex);
+            }	            
+        }
     }
 });
 
