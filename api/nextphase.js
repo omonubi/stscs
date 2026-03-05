17e0a7f6fa46bbe92b9c6f97055525d115aee004
omonubi <omonubi@hotmail.com>
2026-03-05 15:49:45 -0800

    260305.1549 gorn plasma bolts & banners

--- api/nextphase.js
+++ api/nextphase.js
@@ -198,9 +198,8 @@ function displayNewPhase () {
             sortTurnOrder(sorter_asc);
             break;
         case PHASE_1_TARGET: case PHASE_2_TARGET: case PHASE_3_TARGET:
-            sendChat('Phase', `&{template:custom} {{title=**Plasma Bolts**}} {{color=blue}}`);
-            sendChat('Phase', `&{template:custom} {{title=**Tactical Heading Changes**}} {{color=blue}}`);
-            sendChat('Phase', `&{template:custom} {{title=**Engage/Disengage Cloaks**}} {{color=blue}}`);
+            sendChat('Phase', `&{template:custom} {{title=**Tactical Heading Changes?**}} {{color=blue}}`);
+            sendChat('Phase', `&{template:custom} {{title=**Engage/Disengage Cloaks?**}} {{color=blue}}`);
             sendChat('Phase', `&{template:custom} {{title=**Fire / No Fire Selection**}} {{color=blue}}`);
             sortTurnOrder(sorter_asc);
             break;
